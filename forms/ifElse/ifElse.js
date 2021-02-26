411e2d7366334740fa62ed35e854a0319d609851
rileybarry <60715866+rileybarry@users.noreply.github.com>
2021-02-26 15:05:33 -0600

    Complete

---
+++ forms/ifElse/ifElse.js
@@ -0,0 +1,20 @@
+/*
+let firstName = prompt('What is your name?')
+
+let state = prompt('What state are you in?')
+state = state.toUpperCase()
+
+let temp = prompt('What is the temperature in Fahrenheit?')
+
+let messages = ['wear a warm coat, hat, scarf and gloves', 'wear a warm coat but you won\'t need a hat, scarf or gloves.', 'wear your warmest coat, a warm hat, a scarf, and warm gloves.', 'wear a warm coat, hat and gloves. Maybe a scarf too.']
+
+if (temp < 32 && state == 'NE') {
+  console.log(`${firstName}, ${messages[0]}`)
+} else if (50 > temp && temp >= 32 && state == 'NE') {
+  console.log(`${firstName}, ${messages[1]}`)
+} else if (50 > temp && temp >= 32 && state == 'FL') {
+  console.log(`${firstName}, ${messages[2]}`)
+} else if (70 > temp && temp > 50 && state == 'FL') {
+  console.log(`${firstName}, ${messages[3]}`)
+}
+*/
